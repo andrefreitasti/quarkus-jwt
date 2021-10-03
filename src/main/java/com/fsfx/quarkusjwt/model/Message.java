ff5714add304527486c786c129e78e51262a9184
Andre Freitas <andrefreitas.ti@gmail.com>
2021-10-02 23:29:49 -0300

    first commit

---
+++ src/main/java/com/fsfx/quarkusjwt/model/Message.java
@@ -0,0 +1,16 @@
+package com.fsfx.quarkusjwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author ard333
+ */
+@NoArgsConstructor @AllArgsConstructor @ToString
+public class Message {
+	
+	public String content;
+
+}
---
+++ src/main/java/com/fsfx/quarkusjwt/model/Role.java
@@ -0,0 +1,11 @@
+package com.fsfx.quarkusjwt.model;
+
+/**
+ *
+ * @author ard333
+ */
+public enum Role {
+
+	USER, ADMIN
+	
+}
